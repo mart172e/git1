d7626a3ee7e78478e1a3e5e4e96c2b5dd4ffea94
Martin Nielsen <mart172e@edu.eal.dk>
2018-09-26 09:20:22 +0200

    Helper calss tilføjet, og Main i Program rettet

--- git1/Program.cs
+++ git1/Program.cs
@@ -10,12 +10,10 @@ namespace git1
     {
         static void Main(string[] args)
         {
-            // The code provided will print ‘Hello World’ to the console.
-            // Press Ctrl+F5 (or go to Debug > Start Without Debugging) to run your app.
-            Console.WriteLine("Hello World!");
-            Console.ReadKey();
+            Console.WriteLine("hello git1");
+            Console.WriteLine("ny ændring");
+            Console.ReadLine();
 
-            // Go to http://aka.ms/dotnet-get-started-console to continue learning how to build a console app! 
         }
     }
 }
